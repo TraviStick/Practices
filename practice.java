942baaafe5d16cca413a3ec1f29c6c964d1df5e8
TraviStick <lencetravism@gmail.com>
2025-12-19 11:14:29 +0800

    innitial commit

---
+++ practice.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class practice {
+    public static void main(String[] args) {
+        // Bro Code full 12 hr course.
+
+        // 1. Intro to Java
+      //  System.out.println("Abused Child");
+       // System.out.println("Aborted Fetus");
+
+       
+       
+    
+       
+
+
+
+
+
+ // 2. Varialbles & user input
+ /*
+         
+variable = a reusable container for a value
+= a variable behaves as if it was the value it contains
+Primitive simple value stored directly in memory (stack)
+Reference = memory address (stack) that points to the (heap)
+
+Primitive vs Reference
+// int        string
+// double     array
+// char       object
+// boolean
+
+2 steps of variable
+Decleration vs Assignment
+
+
+Scanner sname = new Scanner(System.in);
+System.out.print("Enter your bitch ass name: ");
+int age = 21;             String name = sname.nextLine();
+System.out.print("Enta ya age: ");
+int ager = sname.nextInt();
+double height = 11.5;     
+char grade = 'A';
+boolean passed;
+if (grade == 'A'){
+    passed = true;
+if (passed){
+System.out.println("Your name is: "+name+" and you got an "+grade+" in your class, also you're \"Aborted Fetus\" "+height+", "+age+ager);
+} else {
+    System.out.print(name +" Please Kill yourself");
+}
+}
+sname.close();
+*/
+
+/* 
+// calculate the rectangle area
+double width;
+double height;
+double area;
+
+Scanner sname = new Scanner(System.in);
+
+System.out.print("Enter width: ");
+width = sname.nextDouble();
+
+System.out.print("Enter height: ");
+height = sname.nextDouble();
+
+
+area = width * height;
+
+System.out.print("You got an area of: "+ area + " cm²");
+
+sname.close();
+*/
+
+System.out.println(11<1!=12<2);
+
+    }
+}

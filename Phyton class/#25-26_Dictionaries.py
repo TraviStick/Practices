6d4d8c8fa32a834b20794bbfc559044871247d6a
TraviStick <lencetravism@gmail.com>
2026-06-06 18:22:31 +0800

    s

--- Phyton class/#25-26_Dictionaries.py
+++ Phyton class/#25-26_Dictionaries.py
@@ -78,3 +78,12 @@ for food in cart:
 print("----------------------------Please Pay----------------------------------------")
 print(f"                               ${total}")  
 print("------------------------------------------------------------------------------")
+
+
+
+num = int(input("Enter how tall the pyramid: "))
+
+for i in range(num):
+    spaces = num - i -1
+    stars = 2 * i + 1
+    print(" "*spaces + "*"*stars)
--- Phyton class/tempCodeRunnerFile.py
+++ Phyton class/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-------------------------------------------------------------------------------
+num = int(input("Enter how tall the pyramid: "))
+
+for i in range(num):
+    spaces = num - i -1
+    stars = 2 * i + 1
+    print(" "*spaces + "*"*stars)

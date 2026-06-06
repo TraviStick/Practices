e25a2d40e07dc3abdc21c2b3464771eba261ab79
TraviStick <lencetravism@gmail.com>
2026-06-06 18:22:50 +0800

    s

--- Phyton class/#25-26_Dictionaries.py
+++ Phyton class/#25-26_Dictionaries.py
@@ -78,12 +78,3 @@ for food in cart:
 print("----------------------------Please Pay----------------------------------------")
 print(f"                               ${total}")  
 print("------------------------------------------------------------------------------")
-
-
-
-num = int(input("Enter how tall the pyramid: "))
-
-for i in range(num):
-    spaces = num - i -1
-    stars = 2 * i + 1
-    print(" "*spaces + "*"*stars)

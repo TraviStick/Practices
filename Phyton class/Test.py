24f149259b423571b8a5b6edb02ba5ea2e533c6d
TraviStick <lencetravism@gmail.com>
2026-06-12 19:21:02 +0800

    pls

--- Phyton class/Test.py
+++ Phyton class/Test.py
@@ -1,16 +1,11 @@
-while True:
-    print('Who are you?')
+name = ''
+while not name:
+    print('Enter your name:')
     name = input('>')
-    if name != 'Joe':
-        continue
-    for i in range(3,0,-1):
-        print(f'Hello, Joe. What is the password? (It is a fish.) {i} attempts left.')
-        password = input('>')
-        if i == 0:
-            print('Access Denied')
-        elif password != 'swordfish':
-            print("Try again")
-        else:
-            print('Access Granted.')
-            break
-    break 
+print('How many guests will you have?')
+num_of_guests = int(input('>'))
+if num_of_guests:
+    print('Be sure to have enough room for all your guests.')   
+print('Done')
+print(bool(name))
+print(bool(num_of_guests))
--- Phyton class/tempCodeRunnerFile.py
+++ Phyton class/tempCodeRunnerFile.py
@@ -1,5 +1,16 @@
-for i in range(3,0,-1):
+while True:
+    print('Who are you?')
+    name = input('>')
+    if name != 'Joe':
+        continue
+    for i in range(3,0,-1):
         print(f'Hello, Joe. What is the password? (It is a fish.) {i} attempts left.')
         password = input('>')
-        if password != 'swordfish' and i == 0:
-            continue
+        if i == 0:
+            print('Access Denied')
+        elif password != 'swordfish':
+            print("Try again")
+        else:
+            print('Access Granted.')
+            break
+    break 

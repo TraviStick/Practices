237b50b52a03a62e3a86f7926edaf3d03577da26
TraviStick <lencetravism@gmail.com>
2026-06-14 23:06:18 +0800

    fixed a bug where if the user inputs a single digit the program doesn't print anything

--- Learning Python/Learning Python/ATBS Journey/Chapter4Practice.py
+++ Learning Python/Learning Python/ATBS Journey/Chapter4Practice.py
@@ -1,7 +1,7 @@
 # The Digital Root
 import sys,time
 def digital_sum(number):
-    userLength = len(str(number))
+    userLength = len(str(number)) # using length for which is used to get the individual numbers from first to last
     solve = 0
     for i in range(userLength,0,-1):
         times = 10 ** (i - 1)
@@ -14,9 +14,11 @@ def digital_sum(number):
 
 user_input = int(input('Enter a number: ')) # user input
 
+if len(str(user_input)) <= 1: # only executes if initial input == 1 digit
+    user_input = digital_sum(user_input)
 
 while len(str(user_input)) > 1: # loop
     user_input = digital_sum(user_input)
-else: # only executes if user_input == 1
+else: # only executes if remaining input == 1
     sys.exit()
 

c5098d05ef75b6cce4ca5a11cce71f5ef54de43f
TraviStick <lencetravism@gmail.com>
2026-06-18 17:46:09 +0800

    still learning

--- Learning Python/Test.py
+++ Learning Python/Test.py
@@ -1,22 +1,24 @@
-# The Digital Root
-import sys,time
-def digital_sum(number):
-    userLength = len(str(number))
-    solve = 0
-    for i in range(userLength,0,-1):
-        times = 10 ** (i - 1)
-        result = number // times % 10
-        solve += result
-    print(solve, end=' ')
-    time.sleep(1)
-    return solve
+def box_print(symbol, width, height):
+    if len(symbol) != 1:
+        raise Exception('Symbol must be a single character string.')
+    if width <= 2:
+        raise Exception('Width must be greater than 2.')
+    if height <= 2:
+        raise Exception('Height must be greater than 2.')
 
+    print(symbol * width)
+    for i in range(height - 2):
+        print(symbol + (' ' * (width - 2)) + symbol)
+    print(symbol * width)
 
-user_input = int(input('Enter a number: ')) # user input
-
-
-while len(str(user_input)) > 1: # loop
-    user_input = digital_sum(user_input)
-else: # only executes if user_input == 1
-    sys.exit()
-
+try:
+    box_print('*', 3, 3)
+    box_print('Os', 20, 5)
+    box_print('x', 6, 10)
+    box_print('Z', 10, 6)
+except Exception as err:
+    print('An exception happened: ' + str(err))
+try:
+    box_print('Z', 10, 6)
+except Exception as err:
+    print('An exception happened: ' + str(err))
